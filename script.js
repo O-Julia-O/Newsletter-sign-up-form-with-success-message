2f9bcfc41036aa7f6c3f1c10cc12bb17af0592af
O-Julia-O <julia.qq.onix@gmail.com>
2024-08-14 19:45:02 +0300

    retrieving data from the form

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const formEmail = document.getElementById("c-form");
 
 function handleSubnit(e) {
+    /* prevent the default behavior of the onsubmit event */
     e.preventDefault();
+
+    /* Retrieving data from the form */
+    const data = {};
+    const fields = e.target.querySelectorAll("input, select, textarea");
+
+    for(const field of fields) {
+        data[field.name] = field.value;
+    }
+
+    console.log(data); /* email: "4857485" */
+
 }
 
 formEmail.addEventListener("submit", handleSubnit);
